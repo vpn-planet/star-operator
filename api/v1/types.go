5c1fcfe16b395bdd6fefcd62c05127bf85e1418e
Luma <world@luma.email>
2021-06-27 21:05:08 +0900

    Prepare api/v1/types.go for Device resource implementation

--- api/v1/types.go
+++ api/v1/types.go
@@ -1,10 +1,21 @@
 package v1
 
-// LocalSecretReference contains enough information to let you locate the
-// referenced secret inside the same namespace.
-// Inspired by corev1.LocalObjectReference and corev1.SecretReference
+// LocalSecretReference represents a Secret Reference inside the same
+// namespace. It has enough information to retrieve secret in the
+// same namespace.
 type LocalSecretReference struct {
 	// Name of the referent.
 	// +optional
 	Name string `json:"name,omitempty" protobuf:"bytes,1,opt,name=name"`
+}
+
+// LocalSecretReference represents a Network Reference. It has enough
+// information to retrieve secret in any namespace.
+type NetworkReference struct {
+	// Name of the referent.
+	// +optional
+	Name string `json:"name,omitempty" protobuf:"bytes,1,opt,name=name"`
+	// Namespace of the referent.
+	// +optional
+	Namespace string `json:"namespace,omitempty" protobuf:"bytes,2,opt,name=namespace"`
 }
